2369b30671c6d48e4656262ea8360a99bef75bb7
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-06-18 14:39:22 +0530

    App Layout Structure created Defined Routes

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import {registerLicense} from '@syncfusion/ej2-base';
+
+import App from './App';
+import './index.css';
+
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFacF9JWnxIdkx0RWFbb196dF1MYVVBNQtUQF1hS35Vd01jW3xac3VQRWlc');
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<App/>);
