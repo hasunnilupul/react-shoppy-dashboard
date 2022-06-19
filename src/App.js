de8eaa67966dae622250eb0c6b7dfce675d58664
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-06-19 14:47:50 +0530

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -52,37 +52,37 @@ const App = () => {
                     }
                     <div
                         className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
-                        <div className="fixed md-static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+                        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                             <Navbar/>
                         </div>
-                    </div>
-                    <div>
-                        <Routes>
-                            {/* Dashboard */}
-                            <Route path="/" element={<ECommerce/>}/>
-                            <Route path="/ecommerce" element={<ECommerce/>}/>
+                        <div>
+                            <Routes>
+                                {/* Dashboard */}
+                                <Route path="/" element={<ECommerce/>}/>
+                                <Route path="/ecommerce" element={<ECommerce/>}/>
 
-                            {/* Pages */}
-                            <Route path="/orders" element={<Orders/>}/>
-                            <Route path="/employees" element={<Employees/>}/>
-                            <Route path="/customers" element={<Customers/>}/>
+                                {/* Pages */}
+                                <Route path="/orders" element={<Orders/>}/>
+                                <Route path="/employees" element={<Employees/>}/>
+                                <Route path="/customers" element={<Customers/>}/>
 
-                            {/* Apps */}
-                            <Route path="/kanban" element={<Orders/>}/>
-                            <Route path="/editor" element={<Editor/>}/>
-                            <Route path="/calendar" element={<Calendar/>}/>
-                            <Route path="/color-picker" element={<ColorPicker/>}/>
+                                {/* Apps */}
+                                <Route path="/kanban" element={<Orders/>}/>
+                                <Route path="/editor" element={<Editor/>}/>
+                                <Route path="/calendar" element={<Calendar/>}/>
+                                <Route path="/color-picker" element={<ColorPicker/>}/>
 
-                            {/* Charts */}
-                            <Route path="/line" element={<Line/>}/>
-                            <Route path="/area" element={<Area/>}/>
-                            <Route path="/bar" element={<Bar/>}/>
-                            <Route path="/pie" element={<Pie/>}/>
-                            <Route path="/financial" element={<Financial/>}/>
-                            <Route path="/color-mapping" element={<ColorMapping/>}/>
-                            <Route path="/pyramid" element={<Pyramid/>}/>
-                            <Route path="/stacked" element={<Stacked/>}/>
-                        </Routes>
+                                {/* Charts */}
+                                <Route path="/line" element={<Line/>}/>
+                                <Route path="/area" element={<Area/>}/>
+                                <Route path="/bar" element={<Bar/>}/>
+                                <Route path="/pie" element={<Pie/>}/>
+                                <Route path="/financial" element={<Financial/>}/>
+                                <Route path="/color-mapping" element={<ColorMapping/>}/>
+                                <Route path="/pyramid" element={<Pyramid/>}/>
+                                <Route path="/stacked" element={<Stacked/>}/>
+                            </Routes>
+                        </div>
                     </div>
                 </div>
             </Router>
