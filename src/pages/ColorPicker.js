6d7db2d002f3b3d309c7492b3ee08a6e41400d44
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-06-21 23:15:49 +0530

    Updated design of Color Picker Page

--- src/pages/ColorPicker.js
+++ src/pages/ColorPicker.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {ColorPickerComponent} from "@syncfusion/ej2-react-inputs";
+
+import {Header} from "../components";
 
 const ColorPicker = () => {
+    const onChangeValue = (args) => {
+        document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+    }
+
     return (
-        <div>
-            Color Picker
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header title="Color Picker" category="App"/>
+            <div className="text-center">
+                <div id="preview"/>
+                <div className="flex justify-between items-center gap-20 flex-wrap">
+                    <div>
+                        <p className="text-2xl font-semibold mt-2 mb-4">Inline Palette</p>
+                        <ColorPickerComponent id="inline-palette" mode="Palette" modeSwitcher={false} inline showButtons={false} change={onChangeValue}/>
+                    </div>
+                    <div>
+                        <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+                        <ColorPickerComponent id="inline-palette" mode="Picker" modeSwitcher={false} inline showButtons={false} change={onChangeValue}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
