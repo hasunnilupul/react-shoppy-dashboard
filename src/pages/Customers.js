8e25c7f04e7bf2be84f7feab6ab63946f14f709b
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-06-21 07:05:40 +0530

    Updated design of Customers Page

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {
+    ColumnDirective,
+    ColumnsDirective,
+    Edit,
+    Filter,
+    GridComponent,
+    Inject,
+    Page,
+    Selection,
+    Sort,
+    Toolbar
+} from "@syncfusion/ej2-react-grids";
+
+import {customersData, customersGrid} from '../data/dummy';
+import {Header} from "../components";
 
 const Customers = () => {
     return (
-        <div>
-            Customers
+        <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+            <Header title="Customers" category="Page"/>
+            <GridComponent dataSource={customersData} allowPaging allowSorting
+                           editSettings={{allowDeleting: true, allowEditing: true}} toolbar={['Delete']} width="auto">
+                <ColumnsDirective>
+                    {customersGrid.map((item, index) => (<ColumnDirective key={index} {...item}/>))}
+                </ColumnsDirective>
+                <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]}/>
+            </GridComponent>
         </div>
     );
 };
