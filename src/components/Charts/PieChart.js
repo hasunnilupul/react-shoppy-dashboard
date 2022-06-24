8c368ed77734cc39fd189b778fa601a4bfd32f59
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-06-24 23:39:09 +0530

    Updated design of PieChart component Updated design of Pie page

--- src/components/Charts/PieChart.js
+++ src/components/Charts/PieChart.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import {
+    AccumulationChartComponent,
+    AccumulationDataLabel,
+    AccumulationLegend,
+    AccumulationSeriesCollectionDirective,
+    AccumulationSeriesDirective,
+    AccumulationTooltip,
+    Inject,
+    PieSeries
+} from "@syncfusion/ej2-react-charts";
 
-const PieChart = () => {
+import {useStateContext} from '../../contexts/ContextProvider';
+
+const PieChart = ({id, data, legendVisibility, height}) => {
+    const {currentMode} = useStateContext();
+
     return (
-        <div>
-            Pie Chart
-        </div>
+        <AccumulationChartComponent
+            id="pie-chart"
+            legendSettings={{visibility: legendVisibility, background: 'white'}}
+            height={height}
+            background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+            tooltip={{enable: true}}>
+            <Inject services={[PieSeries, AccumulationLegend, AccumulationTooltip, AccumulationDataLabel]}/>
+            <AccumulationSeriesCollectionDirective>
+                <AccumulationSeriesDirective
+                    name="Sale"
+                    dataSource={data}
+                    xName="x"
+                    yName="y"
+                    innerRadius="40%"
+                    startAngle={0}
+                    endAngle={360}
+                    radius="70%"
+                    explode
+                    explodeOffset="10%"
+                    explodeIndex={2}
+                    dataLabel={{
+                        visible: true,
+                        name: 'text',
+                        position: 'Inside',
+                        font: {
+                            fontWeight: '600',
+                            color: '#fff',
+                        },
+                    }}/>
+            </AccumulationSeriesCollectionDirective>
+        </AccumulationChartComponent>
     );
 };
 
--- src/pages/Charts/Pie.js
+++ src/pages/Charts/Pie.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import {pieChartData} from '../../data/dummy';
+import {Header, PieChart} from "../../components";
+
 const Pie = () => {
     return (
-        <div>
-            Pie
+        <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+            <Header title="Project Cost Breakdown" category="Pie"/>
+            <div className=" w-full">
+                <PieChart id="chart-pie" data={pieChartData} legendVisibility height="full"/>
+            </div>
         </div>
     );
 };
