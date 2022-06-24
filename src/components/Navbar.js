24451c96b155556fc12df388ce28bf7d50386d94
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-06-24 23:03:47 +0530

    Updated design of Bar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,7 +59,7 @@ const Navbar = () => {
                         <img src={avatar} className="rounded-full w-8 h-8" alt="user-avatar"/>
                         <p>
                             <span className="text-gray-400 text-14">Hi,</span>
-                            <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+                            <span className="text-gray-400 font-bold ml-1 text-14">John</span>
                         </p>
                         <MdKeyboardArrowDown className="text-gray-400 text-14"/>
                     </div>
--- src/pages/Charts/Bar.js
+++ src/pages/Charts/Bar.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {
+    Category,
+    ChartComponent,
+    ColumnSeries,
+    DataLabel,
+    Inject,
+    Legend,
+    SeriesCollectionDirective,
+    SeriesDirective,
+    Tooltip
+} from "@syncfusion/ej2-react-charts";
+
+import {Header} from "../../components";
+import {barCustomSeries, barPrimaryXAxis, barPrimaryYAxis} from "../../data/dummy";
+import {useStateContext} from "../../contexts/ContextProvider";
 
 const Bar = () => {
+    const {currentMode} = useStateContext();
+
     return (
-        <div>
-            Bar
+        <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+            <Header category="Bar" title="Olympic Medal Counts - RIO"/>
+            <div className=" w-full">
+                <ChartComponent id="bar-chart" height="420px" primaryXAxis={barPrimaryXAxis}
+                                primaryYAxis={barPrimaryYAxis}
+                                chartArea={{border: {width: 0}}} tooltip={{enable: true}}
+                                background={currentMode === 'Dark' ? '#33373E' : '#FFF'}
+                                legendSettings={{background: 'white'}}>
+                    <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]}/>
+                    <SeriesCollectionDirective>
+                        {barCustomSeries.map((item, index) => (<SeriesDirective key={index} {...item}/>))}
+                    </SeriesCollectionDirective>
+                </ChartComponent>
+            </div>
         </div>
     );
 };
